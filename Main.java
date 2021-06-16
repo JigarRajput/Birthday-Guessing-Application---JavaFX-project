5c3ffb1388820e008955958024ea4ad29ae0421b
JigarRajput <80737219+JigarRajput@users.noreply.github.com>
2021-06-16 22:13:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
-//import javafx.scene.layout.BorderPane;
+
 
 
 public class Main extends Application {
@@ -15,7 +15,7 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("fxdoc.fxml"));
 			
 			Scene scene = new Scene(root);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
